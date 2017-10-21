ae6c0e010773de3d0fea5342905fa4b1112d7fb9
Karl Wolf <wolfkarl@gmail.com>
2017-10-21 15:40:14 +0200

    fix column even more

--- db/migrate/20171020102735_add_parent_id_to_category.rb
+++ db/migrate/20171020102735_add_parent_id_to_category.rb
@@ -1,5 +1,5 @@
 class AddParentIdToCategory < ActiveRecord::Migration
   def change
-    change_column :categories, :parent_category_id, 'integer USING CAST(column_name AS integer)'
+    change_column :categories, :parent_category_id, 'integer USING CAST(parent_category_id AS integer)'
   end
 end
