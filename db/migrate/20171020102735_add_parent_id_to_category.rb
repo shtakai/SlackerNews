f86754baeb5a05fb12f8239deed0e6e9835277d6
Karl Wolf <wolfkarl@gmail.com>
2017-10-21 15:33:11 +0200

    try fixing id migration

--- db/migrate/20171020102735_add_parent_id_to_category.rb
+++ db/migrate/20171020102735_add_parent_id_to_category.rb
@@ -1,5 +1,5 @@
 class AddParentIdToCategory < ActiveRecord::Migration
   def change
-    change_column :categories, :parent_category_id, :integer
+    change_column :categories, :parent_category_id, 'integer USING CAST(column_name AS integer)'
   end
 end
