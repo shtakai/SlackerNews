538542f4d65a47a189814b49dd5086a13852b7a8
Karl Wolf <wolfkarl@gmail.com>
2017-11-17 15:39:09 +0100

    route refactoring, sort via scopes + controller function

--- config/routes.rb
+++ config/routes.rb
@@ -1,84 +1,42 @@
 Rails.application.routes.draw do
-  resources :posts do
-    member do
-      get 'upvote' => 'posts#upvote'
-      get 'downvote' => 'posts#downvote'
 
-      get 'favour' => 'posts#favour'
-      get 'unfavour' => 'posts#unfavour'
-    end
-    collection do
-      get 'best' => 'posts#index_best'
-      get 'hot' => 'posts#index_hot'
-    end
-    resources :comments
-  end
-
-  get 'subscriptions' => 'posts#subscriptions', :as => :subscriptions_posts
-  get 'user/:id/posts' => 'posts#user_posts', :as => :user_posts
-  get 'user/:id/favourites' => 'posts#user_favourites', :as => :user_favourites
-
-  resources :categories do
-    member do
-      get 'subscribe' => 'categories#subscribe'
-      get 'unsubscribe' => 'categories#unsubscribe'
-    end
-  end
-  
-  devise_for :users
-  resources :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'posts#index'
-  get 's/:cat' => 'posts#category', :as => 'category_posts'
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  constraints ValidSortOrder do
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    get 'subscriptions/(:sortby)' => 'posts#subscriptions', :as => :subscriptions_posts
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    resources :posts, :path => '/' do
+      member do
+        get 'upvote' => 'posts#upvote'
+        get 'downvote' => 'posts#downvote'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+        get 'favour' => 'posts#favour'
+        get 'unfavour' => 'posts#unfavour'
+      end
+      collection do
+        get '(:sortby)' => 'posts#index'
+      end
+      resources :comments
+    end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+    resources :categories, :path => 'c' do
+      member do
+        get 'subscribe' => 'categories#subscribe'
+        get 'unsubscribe' => 'categories#unsubscribe'
+        get '(:sortby)' => 'posts#category'
+      end
+    end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    devise_for :users
+    resources :users do
+      member do
+        get 'posts(/:sortby)' => 'posts#user_posts', :as => :user_posts
+        get 'favourites(/:sortby)' => 'posts#user_favourites', :as => :user_favourites
+      end
+    end
+
+  end
+
 end
