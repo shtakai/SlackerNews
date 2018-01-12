7a6a172d221f45f9e27eb3ec2d9ad66507e4aadd
paul <paul.wille@t-online.de>
2018-01-12 18:55:52 +0100

    change ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -76,4 +76,4 @@ set :user, 'slacker'
 set :environment, :production
 set :branch, :master
 set :domain, 'slackernews.de'
-set :ruby_version, '2.5.0'  # default is '2.3.1', can be set to every ruby version supported by uberspace.
+set :ruby_version, '2.2.3'  # default is '2.3.1', can be set to every ruby version supported by uberspace.
