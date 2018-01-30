c9ee576d13df5f35d4514b5d3046b622ef9d81d8
Karl Wolf <wolfkarl@gmail.com>
2018-01-31 00:09:41 +0100

    make cast migration of death adapter specific

--- db/migrate/20171020102735_add_parent_id_to_category.rb
+++ db/migrate/20171020102735_add_parent_id_to_category.rb
@@ -1,5 +1,11 @@
 class AddParentIdToCategory < ActiveRecord::Migration
   def change
-    change_column :categories, :parent_category_id, 'integer USING CAST(parent_category_id AS integer)'
+    adapter_type = connection.adapter_name.downcase.to_sym
+    case adapter_type
+      when :postgresql
+        change_column :categories, :parent_category_id, 'integer USING CAST(parent_category_id AS integer)'
+      else
+        change_column :categories, :parent_category_id, 'integer'
+    end
   end
 end
